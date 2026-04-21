2848d4e895825277d5e135f140918ce9b32b87f1
xinbreak <maksimmatiukhin05@gmail.com>
2026-04-21 20:40:55 +0300

    feat: add search bar functionality

--- client/src/components/UI/SearchBar/SearchBar.tsx
+++ client/src/components/UI/SearchBar/SearchBar.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect, type ChangeEvent } from 'react'
 import styles from './SearchBar.module.css'
 import searchIcon from '../../../assets/icons/Search.svg'
 
 export default function SearchBar() {
+  const [query, setQuery] = useState<string>('')
+
+  useEffect(() => {
+    if (!query.trim()) return
+
+    const handler = setTimeout(() => {
+      fetchData(query)
+    }, 800)
+
+    return () => clearTimeout(handler)
+  }, [query])
+
+  const fetchData = (value: string) => {
+    console.log(value)
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value)
+  }
+
   return (
     <div className={styles.searchBar}>
       <img src={searchIcon} alt="Search" className={styles.searchIcon} />
@@ -9,6 +30,8 @@ export default function SearchBar() {
         type="text"
         className={styles.searchInput}
         placeholder="Search any movie or serie"
+        value={query}
+        onChange={handleChange}
       />
     </div>
   )
